321eb406fd29f04332af67f409dfdef38c7449be
JohnnyHHub <jhoang31@horizon.csueastbay.edu>
2024-11-01 21:48:51 -0700

    Create MainHub.java

---
+++ src/main/java/gui/MainHub.java
@@ -0,0 +1,20 @@
+package main.java.gui;
+
+import javax.swing.*;
+
+/*
+ * TODO: create buttons and JList
+ * add buttons and JList to panels.
+ * add panels to JFrame
+ * add functionalities
+ *  
+ */
+public class MainHub {
+	JFrame mainFrame = new JFrame();
+	JPanel chatContainer = new JPanel();
+	JPanel buttonContainer = new JPanel();
+	
+	public MainHub() {
+		mainFrame.setLayout(new BoxLayout(null, 0));
+	}
+}
