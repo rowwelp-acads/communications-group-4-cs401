3c3690a498fed2fa56b1aa76b277c035fb05b091
John <jhuynh71@horizon.csueastbay.edu>
2024-11-21 14:30:48 -0800

    Conversation History/Log

---
+++ src/main/java/ConversationLog.java
@@ -0,0 +1,28 @@
+package main.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConversationLog {
+
+	private String logId;
+	private List<ConversationHistory> allHistories;
+	
+	public ConversationLog(String logId) {
+		this.logId = logId;
+		allHistories = new ArrayList<ConversationHistory>();
+	}
+	
+	public String getLogId() {
+		return logId;
+	}
+	
+	public void addConversationHistory(ConversationHistory hist) {
+		allHistories.add(hist);
+	}
+	
+	public List<ConversationHistory> getAllHistories() {
+		return allHistories;
+	}
+	
+}
