81c6e6a3d4d78f5f650114038a6e8742276a9bac
Rowwel Ponesto <rponesto@horizon.csueastbay.edu>
2024-11-21 02:04:36 -0800

    Update Chat.java
    Zombie-implemented the methods.
    
    Adding a functioning addPartcipant method requires the userAccount class to be polished, don't want to pull it from another branch aside from the main branch (i don't know the consequences)
    
    Same problems with removeParticipant, there is currently no established way to communicate to the server which user to remove from that chat and how the Server will handle this, but it has been implemented client-side
    
    sendMessage is not implemented yet.
    
    loadConversationHistory is partially implemented. See comment in the code.

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -2,8 +2,6 @@ package main.java;
 
 import java.util.*;
 
-
-/*
 public class Chat {
 	private String chatID;
 	private List<UserAccount> participants;
@@ -16,20 +14,100 @@ public class Chat {
 		// Go through participant database
 		// if username == UserAcount.getUsername()
 		
+		for (UserAccount participant : participants) {
+            if (participant.getUsername().equals(username)) {
+                System.out.println("User " + username + " is already a participant.");
+                return;
+            }
+        }
+		
 		// Somehow call server's UserManagement to get the useraccount based on the userID
+		// Use a Message object to send UserAccount over the network? Must somehow find a way 
+		// to pass UserAccount to client or construct a new one based on a Message passed over the network.
+		
+		/* Something along these lines...
+		UserAccount user = userManagement.getUserByUsername(username);
+        if (user != null) {
+            // Add the user to the participants list
+            participants.add(user);
+            System.out.println("User " + username + " added to the chat.");
+        } else {
+            System.out.println("User " + username + " does not exist.");
+        }
+		*/
 		
 		
 	}
 	public void removeParticipant(String username) {
 		// Implement it here...
 		
+	    UserAccount userToRemove = null;
+	    for (UserAccount user : participants) {
+	        if (user.getUsername().equals(username)) {
+	            userToRemove = user;
+	            break;
+	        }
+	    }
+	    
+	    if (userToRemove != null) {
+	        participants.remove(userToRemove);
+	        activeParticipants.remove(userToRemove);
+	        
+	        String systemMessageContent = username + " has left the chat.";
+	        Message systemMessage = new Message(systemMessageContent, null); // System messages may not have a sender
+	        messages.add(systemMessage);
+	        
+	        System.out.println("User " + username + " removed from chat.");
+	    } else {
+	        System.out.println("User " + username + " not found in participants.");
+	    }
+		
 	}
 	public void sendMessage(Message message) {
 		// Implement it here...
+		
 	}
 	
 	public List<Message> loadConversationHistory() {
 		// Implement it here...
+		// String filePath = chatID + ".txt";
+		
+		// For testing:
+		String filePath = "conversationhistory.txt";
+		
+		List<Message> messages = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\\|", 3); // Split into at most 3 parts
+                if (parts.length == 3) {
+                    String sender = parts[0];       // First part: sender
+                    String timestamp = parts[1];   // Second part: timestamp
+                    String content = parts[2];     // Third part: full message content
+                    
+                    /*
+                    This might change since right now the only things I have to be saved
+                    in the conversationhistory text file is their username, time and date sent
+                    and the message content.
+                    
+                    When loading the textfile not all userAccount info is stored on there, but
+                    creating a Message requires a userAccount as a "sender". Thus a userAccount
+                    must be created here based on the username stored in the file. I don't really
+                    know how to deal with this entirely yet and I don't want to make major changes
+                    to the way the program works without first consulting the team.
+                     */
+                    
+                    UserAccount senderAccount = new UserAccount(sender, sender, "", "");
+                    
+                    // Create the Message object
+                    Message message = new Message(content, senderAccount);
+                    
+                    messages.add(message);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return messages;
 	}
 }
-*/
