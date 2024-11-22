f794675556771124ba583a9500f8cc2fcb2a3525
kai-csueb <kabillar@horizon.csueastbay.edu>
2024-11-21 18:25:09 -0800

    Update ConversationHistory
    - removed a few methods

--- src/main/java/ConversationHistory.java
+++ src/main/java/ConversationHistory.java
@@ -4,32 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConversationHistory {
-	private String userId;
+	private List<String> Messages;
 	
-	
-	public ConversationHistory(String userId) {
-		this.userId = userId;
-		chats = new ArrayList<Chat>();
+	public ConversationHistory() {
+		Messages = new ArrayList<>();
 	}
+
 	
-	public String getUserId() {
-		return userId;
-	}
-	
-	public void addChat(Chat newChat) {
-		chats.add(newChat);
-	}
-	
-	public void removeChat(Chat removeChat) {
-		chats.remove(removeChat);
-	}
+
 	
 	/*
 	 * not sure if this should be here, delete if not
 	 */
-	public List<Message> getLatestMessages(String chatId) {
-		//update method if it does not match with chat loadconvo command
-		return chats.get(chatId).loadConversationalHistory();
-	}
-	
+//	public List<Message> getLatestMessages(String chatId) {
+//		//update method if it does not match with chat loadconvo command
+//		return chats.get(chatId).loadConversationalHistory();
+//	}
+//	
 }
