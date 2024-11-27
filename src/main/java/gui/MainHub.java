8f493102dc1fc3b79d3b3392e08a1dc1db482402
kai-csueb <kabillar@horizon.csueastbay.edu>
2024-11-26 17:19:18 -0800

    Update MainHub.java
    - Reorganized
    - added UserAccount
    - modified Constructor
    - STILL NEED TO CHANGE CREATE CHAT AND DELETE CHAT
    - modified JList to display ChatList

--- src/main/java/gui/MainHub.java
+++ src/main/java/gui/MainHub.java
@@ -11,6 +11,7 @@ import javax.swing.event.ListSelectionListener;
 
 import main.java.ChatList;
 import main.java.Message;
+import main.java.UserAccount;
 
 
 /*
@@ -24,6 +25,8 @@ import main.java.Message;
  *  
  */
 public class MainHub {
+	
+	// GUI PROPERTIES
 	JFrame mainFrame = new JFrame("Main Hub");
 	JButton itButton = new JButton("IT Button");
 	JButton createPrivateChatButton = new JButton("Create Private Group Chat");
@@ -33,49 +36,66 @@ public class MainHub {
 	JButton logoutButton = new JButton("Logout");
 	JPanel buttonContainer = new JPanel();
 	JPanel chatsContainer = new JPanel();
+	
+	// STREAMS
 	ObjectInputStream msgIn;
 	ObjectOutputStream msgOut;
+	
+	// MAIN HUB PROPERTIES
 	ChatList userChatList;
 	String selectedChatRoom = "";
+    private UserAccount owner; // NOVEMBER 26
 	
 	
-	String chatExample[] = {"2nd Floor group chat",
-							"Engineering department group chat",
-							"IT department group chat",
-							"Coworker-1 private chat",
-							"Manager private chat"};
+    // DELETED NOVEMBER 26 KA
+//	String chatExample[] = {"2nd Floor group chat",
+//							"Engineering department group chat",
+//							"IT department group chat",
+//							"Coworker-1 private chat",
+//							"Manager private chat"};
 	
-	// Chatroom chatroom  
+
+
 	
 	// default constructor
 	public MainHub() {	
 	}
-			
-	public MainHub(ObjectInputStream in, ObjectOutputStream out) {
+	
+	// MODIFIED NOVEMBER 26 KA
+	// ADDED USER ACCOUNT
+	public MainHub(ObjectInputStream in, ObjectOutputStream out, UserAccount userAccount) {
 		msgIn = in;
 		msgOut = out;
+		owner = userAccount;
+		userChatList = owner.getChatList();  // Get user's ChatList
 	}
 	
 	public void openMainHub() {
 		
-		JList chatList = new JList(chatExample);
-		chatsContainer.add(new JScrollPane(chatList));
+		// CHAT LIST
+		// MODIFIED NOVEMBER 26
+		JList chatList = new JList(userChatList.getChatListForDisplay());
+        chatsContainer.add(new JScrollPane(chatList));
+		chatList.addListSelectionListener(new ChatListSelectionListener());
 		
-		buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.X_AXIS));
-		
+		// GUI COMPONENTS
 		itButton.setEnabled(false);
+		buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.X_AXIS));
 		buttonContainer.add(itButton);
-		
 		buttonContainer.add(createPrivateChatButton);
 		buttonContainer.add(createGroupChatButton);
 		buttonContainer.add(deleteChatButton);
 		buttonContainer.add(openChatButton);
 		buttonContainer.add(logoutButton);
 		
-		
 		mainFrame.setLayout(new BoxLayout(mainFrame.getContentPane(), BoxLayout.Y_AXIS));
 		mainFrame.add(chatsContainer);
 		mainFrame.add(buttonContainer);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.pack();
+		mainFrame.setAlwaysOnTop(true);
+		mainFrame.setVisible(true);
+		mainFrame.setLocationRelativeTo(null);
 		
 		EventListener event = new EventListener();
 		itButton.addActionListener(event);
@@ -84,17 +104,9 @@ public class MainHub {
 		deleteChatButton.addActionListener(event);
 		openChatButton.addActionListener(event);
 		logoutButton.addActionListener(event);
-		chatList.addListSelectionListener(new ChatListSelectionListener());
 		
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mainFrame.pack();
-		mainFrame.setAlwaysOnTop(true);
-		mainFrame.setVisible(true);
-		mainFrame.setLocationRelativeTo(null);
-		
-		
+		// MAIN HUB THREAD
 		new Thread(new Runnable() {
-
 			@Override
 			public void run() {
 				while (true) {
@@ -121,57 +133,56 @@ public class MainHub {
 		}).start();
 	}
 	
+	// BUTTON LISTENERS
+	
 	private class EventListener implements ActionListener {
-
 		@Override
 		public void actionPerformed(ActionEvent event) {
+			
+			// LOGOUT
 			if (event.getActionCommand().equals("logout")){
-					// logout method
-					JOptionPane.showMessageDialog(mainFrame, "Placeholder. Logout method will be call here", "Info", JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(mainFrame, "Placeholder. Logout method will be call here", "Info", JOptionPane.INFORMATION_MESSAGE);
 	            }
+			
+			// OPEN CHAT
 			else if (event.getActionCommand().equals("Open Chat")) {
-				// open chat method
-				//JOptionPane.showMessageDialog(mainFrame, "Placeholder. Open chat method will be call here", "Info", JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(mainFrame, "Placeholder. Open chat method will be call here", "Info", JOptionPane.INFORMATION_MESSAGE);
 				ChatRoom chatRoom = new ChatRoom(msgIn, msgOut);
 				mainFrame.setVisible(false);
 			}
+			
+			// DELETE CHAT
 			else if (event.getActionCommand().equals("Delete Chat")) {
-				// delete chat method
 				JOptionPane.showMessageDialog(mainFrame, "Placeholder. Delete chat method will be call here", "Info", JOptionPane.INFORMATION_MESSAGE);
 			}
+			
+			// CREATE GROUP CHAT
 			else if (event.getActionCommand().equals("Create Group Chat")) {
 				// create group chat method
 				JOptionPane.showMessageDialog(mainFrame, "Placeholder. Create group chat method will be call here", "Info", JOptionPane.INFORMATION_MESSAGE);
 			}
+			
+			// CREATE PRIVATE CHAT
 			else if (event.getActionCommand().equals("Create Private Group Chat")) {
-				// create private group chat method
 				JOptionPane.showMessageDialog(mainFrame, "Placeholder. Create private group chat method will be call here", "Info", JOptionPane.INFORMATION_MESSAGE);
 			}
+			
+			// OPEN IT BUTTON
 			else if (event.getActionCommand().equals("IT Button")) {
 				// IT Button method
 				JOptionPane.showMessageDialog(mainFrame, "Placeholder. IT button method will be call here", "Info", JOptionPane.INFORMATION_MESSAGE);
 			}
-			
 		}
-		
 	}
 	
+	// JLIST LISTENER
 	private class ChatListSelectionListener implements ListSelectionListener {
-
-		
 		@Override
 		public void valueChanged(ListSelectionEvent e) {
 	        if (!e.getValueIsAdjusting()) { // Avoid multiple events for the same selection
 	            JList source = (JList) e.getSource();
 	            selectedChatRoom = (String) source.getSelectedValue();
 	        }
-			
 		}
-		
 	}
-	/*
-	public static void main(String[] args) {
-		MainHub mainhub = new MainHub();
-	}
-	*/
 }
