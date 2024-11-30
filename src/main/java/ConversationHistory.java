fe5b2b8a9ba01fe0492a61cde3c6c7944796062e
John <jhuynh71@horizon.csueastbay.edu>
2024-11-29 19:52:17 -0800

    convo history

--- src/main/java/ConversationHistory.java
+++ src/main/java/ConversationHistory.java
@@ -1,5 +1,8 @@
 package main.java;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,10 +10,19 @@ import java.util.List;
 public class ConversationHistory implements Serializable {
 	private int chatID;
 	private List<String> Messages;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
 	
 	public ConversationHistory(int id) {
 		this.chatID = id;
 		Messages = new ArrayList<>();
+	}
+	
+	public ConversationHistory(int id, ObjectInputStream in, ObjectOutputStream out) {
+		this.chatID = id;
+		this.in = in;
+		this.out = out;
+		load(id);
 	}
 	
 	public int getChatID() {
@@ -32,6 +44,21 @@ public class ConversationHistory implements Serializable {
 	
 	//for pre-existing chat
 	public void load(int chatID) {
+		
+    	// SENDING A MESSAGE TO SERVER //
+    	
+    	try {
+            Message request = new Message(chatID, MESSAGETYPE.GETHISTORY);
+            out.writeObject(request);
+            out.flush();
+            
+            Message response = (Message) in.readObject();
+            if (response.getType() == MESSAGETYPE.SENDHISTORY) {
+            	Messages = response.getChatHistory();
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Error updating chat list: " + e.getMessage());
+        }
 		
 	}
 	
