e59922c989a21c9938fc59f57b2bbe557fa1ea67
John <jhuynh71@horizon.csueastbay.edu>
2024-11-29 19:05:40 -0800

    test convo log

--- src/main/java/ConversationHistory.java
+++ src/main/java/ConversationHistory.java
@@ -1,12 +1,8 @@
 package main.java;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class ConversationHistory implements Serializable {
 	private int chatID;
@@ -21,92 +17,24 @@ public class ConversationHistory implements Serializable {
 		return chatID;
 	}
 	
-	public void addMessageToHistory(Message newMessage) {
+	public void addMessage(Message newMessage) {
 		String message = newMessage.getSender() + "|" + newMessage.getTimestamp() + "|" + newMessage.getContent();
 		Messages.add(message);
+	}
+	
+	public void addMessage(String newMessage) {
+		Messages.add(newMessage);
 	}
 		
 	public List<String> getMessageList() {
 		return Messages;
 	}
 	
-	public void write() {
-		String chatFile = Integer.toString(chatID);
-		String fileName = chatFile.concat(".txt");
-		
-		FileWriter myWriter;
-		
-		try {
-			myWriter = new FileWriter(fileName);
-			
-			for (int i = 0; i < Messages.size(); i++) {
-			myWriter.write(Messages.get(i));
-			}
-			
-			myWriter.close();
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
 	public void load() {
 		
-		String chatFile = Integer.toString(chatID);
-		String filename = chatFile.concat(".txt");
-		
-    	File openfile = new File(filename);
-    	
-    	try {
-    		//prevents opening a file that doesn't exist
-			if (openfile.createNewFile()) return;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    	
-        try {
-            Scanner scanner = new Scanner(openfile);
-            while (scanner.hasNextLine()) {
-            	String data = scanner.nextLine();
-            	Messages.add(data);
-            }
-            
-            scanner.close();
-        } catch (Exception e) {
-        System.out.println(e);
-        }
+		//to do
 
 	}
 	
-	public void load(int newID) {
-		
-		chatID = newID;
-		
-		String chatFile = Integer.toString(chatID);
-		String filename = chatFile.concat(".txt");
-		
-    	File openfile = new File(filename);
-    	
-    	try {
-    		//prevents opening a file that doesn't exist
-			if (openfile.createNewFile()) return;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-    	
-        try {
-            Scanner scanner = new Scanner(openfile);
-            while (scanner.hasNextLine()) {
-            	String data = scanner.nextLine();
-            	Messages.add(data);
-            }
-            
-            scanner.close();
-        } catch (Exception e) {
-        System.out.println(e);
-        }
-		
-	}
 	
 }
--- src/main/java/ConversationLog.java
+++ src/main/java/ConversationLog.java
@@ -1,9 +1,13 @@
 package main.java;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 public class ConversationLog {
@@ -19,7 +23,7 @@ public class ConversationLog {
 	}
 	
 	public void addMessageToLog(int chatID, Message newMessage) {
-		allHistories.get(chatID).addMessageToHistory(newMessage);;
+		allHistories.get(chatID).addMessage(newMessage);;
 	}
 	
 	public List<Integer> displayList() {
@@ -39,6 +43,58 @@ public class ConversationLog {
 	
 	public boolean doesItExist(int chatID) {
 		return allHistories.containsKey(chatID);
+	}
+	
+	
+	public void write(int chatID) {
+		if (!this.doesItExist(chatID)) return;
+		
+		String chatFile = Integer.toString(chatID);
+		String fileName = chatFile.concat(".txt");
+		
+		List<String> history = allHistories.get(chatID).getMessageList();
+		
+		FileWriter myWriter;
+		
+		
+		try {
+			myWriter = new FileWriter(fileName);
+			
+			for (int i = 0; i < history.size(); i++) {
+			myWriter.write(history.get(i));
+			}
+			
+			myWriter.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public List<String> load(int chatID) {
+		
+		String chatFile = Integer.toString(chatID);
+		String filename = chatFile.concat(".txt");
+		
+		List<String> history = new ArrayList<>();
+		
+		File openfile = new File(filename);
+    	
+        try {
+            Scanner scanner = new Scanner(openfile);
+            while (scanner.hasNextLine()) {
+            	String data = scanner.nextLine();
+            	history.add(data);
+            }
+            
+            scanner.close();
+        } catch (Exception e) {
+        System.out.println(e);
+        }
+		
+        return history;
+        
 	}
 	
 }
