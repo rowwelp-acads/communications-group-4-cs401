5115db1e4f1fc93031672ab89ecdf0f8c10db09b
John <77904017+Johnguin@users.noreply.github.com>
2024-12-03 18:38:13 -0800

    Update ConvoLogTest.java

--- src/testing/ConvoLogTest.java
+++ src/testing/ConvoLogTest.java
@@ -14,13 +14,53 @@ class ConvoLogTest {
 	void testConvoLog() {
 		int chatID = 1;
 		
-		Message testMes = new Message();
 		ConversationHistory testHist = new ConversationHistory(chatID);
 		ConversationLog testLog = new ConversationLog();
 		
+		testLog.addConversationHistory(testHist);
+		
+		assertTrue(testLog.getHistory(chatID).getChatID() == testHist.getChatID());
+	}
+	
+	@Test
+	void testConvoDisplayList() {
+		int chatID = 1;
+		int secChat = 2;
+		
+		ConversationHistory testHist = new ConversationHistory(chatID);
+		ConversationHistory testHist2 = new ConversationHistory(secChat);
+		ConversationLog testLog = new ConversationLog();
+		
+		testLog.addConversationHistory(testHist);
+		testLog.addConversationHistory(testHist2);
 		
 		
-		assertTrue(true);
+		assertTrue(testLog.displayList().get(0) == chatID && testLog.displayList().get(1) == secChat);
+		
+	}
+	
+	@Test
+	void testConversationHistory() {
+		int chatID = 1;
+		
+		Message testMes = new Message("John", "pass");
+		
+		ConversationHistory testHist = new ConversationHistory(chatID);
+		testHist.addMessage(testMes);
+		
+		String test = testMes.getTimestamp() + " || " + testMes.getSender().getUsername() + ": " + testMes.getContent();
+		
+		assertTrue(test.equals(testHist.getMessageList().get(0)));
+	}
+	
+	@Test
+	void testConvoHistoID() {
+		int chatID = 1;
+		
+		ConversationHistory testHist = new ConversationHistory(chatID);
+		
+		assertTrue(testHist.getChatID() == chatID);
+		
 	}
 
 }
