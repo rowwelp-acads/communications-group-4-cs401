364de9fe0186a43be32d1ab0349525315b005f2d
Rowwel Ponesto <rponesto@horizon.csueastbay.edu>
2024-12-03 23:16:40 -0800

    Update AllTests.java
    Added remaining tests to the Selected test classes

--- src/testing/AllTests.java
+++ src/testing/AllTests.java
@@ -7,7 +7,10 @@ import org.junit.platform.suite.api.Suite;
 @SelectClasses({
 	UserAccount_Testing.class,
 	ConvoLogTest.class,
-	chatTest.class
+	chatTest.class,
+	ChatList_Testing.class,
+	ChatListManager_Testing.class,
+	ServerTest.class
 })
 
 public class AllTests {
